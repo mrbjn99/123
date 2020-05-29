07db1db7243dcb67b2a9bf048b455fd969bdd0e1
mrbjn99 <58242504+mrbjn99@users.noreply.github.com>
2020-05-29 16:55:10 +0700

    them style

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<html>
+    <head>
+        <title>Hello World</title>
+    </head>
+    <body>
+        <h1> I'm cat</h1>
+        <h2> Hello human</h2>
+        <h3> List i liked :</h3>
+        <ul>
+            <li> Fish </li>
+            <li> Pate </li>
+            <li> Human meat </li>
+        </ul>
+    </body>
+</html>
